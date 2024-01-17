72eb9e9b2c7c6f7ab6b1a26b3c23af0055cdf2c1
pc01\aluno <informatica@eticalgarve.com>
2024-01-17 19:28:35 +0000

    1ro commit

---
+++ aula2/app.js
@@ -0,0 +1,47 @@
+//function menu2() {
+    //console.log(b)
+
+//}
+
+//let a = "praia"
+//let b = "piscina"
+//let c = "agua"
+//console.log(a)
+//console.log(b)
+//console.log(c)
+
+//menu2()
+
+//////////////////////////////////////
+
+//const menu2 = () => {
+
+  //  let a = "praia"
+
+   // return a
+//}
+ //console.log(menu2())
+
+//const menu3 = () => {
+    //let a = "campo"
+
+   // return a
+//}
+//let a = "carro"
+//console.log(menu3())
+//console.log(a)
+
+//let d = "vida"
+//console.log(d)
+
+////////////////////////////////////////
+
+const adNumbers = (a, b) => {
+
+    return a + b
+}
+
+let a = 1
+let b = 2
+
+console.log(adNumbers(a,b))
