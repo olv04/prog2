796464e87c0f2cf8351bcb7a6bcb1ff8c870419f
pc01\aluno <informatica@eticalgarve.com>
2024-01-18 19:17:19 +0000

    2do commit

---
+++ aula3/app.js
@@ -0,0 +1,26 @@
+/*let a = "praia"
+
+ if(a == "praia") {
+     console.log(true)
+  } else {
+     console.log(false)
+  }
+
+/*switch (a) {
+    case 10
+    console.log(true)
+    break
+    case "10"
+    console.log(false)
+}
+*/
+
+let a = 0
+
+
+while (a < 100) {
+
+      console.log(a)
+
+    a = a + 1
+}
