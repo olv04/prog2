e36e3f9c40dd8f0ec43d5dbb5e6fbf3a6b09fceb
pc01\aluno <informatica@eticalgarve.com>
2024-01-19 19:52:57 +0000

    3ro commit

---
+++ aula5/app.js
@@ -0,0 +1,14 @@
+// let user = {
+//     id: 0,
+//     name: "tomas",
+//     age: 20,
+//     gender: "male",
+//     dob: "14-03-2004",
+//     hobbie: ("musica", "passear", "code")
+// }
+
+import Car from "./Car.js"
+import Motorcycle from "./Motorcycle.js"
+
+const car = new Car()
+const motorcycle = new Motorcycle()
