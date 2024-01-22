90cc8c3d0b7a5731567fcc8730194c93b4eb6e42
pc01\aluno <informatica@eticalgarve.com>
2024-01-22 19:09:36 +0000

    4to commit

---
+++ aula6/Car.js
@@ -0,0 +1,14 @@
+import Vehicle from "../../Vehicle.js"
+
+export default class Car extends Vehicle {
+
+    constructor() {
+        super()
+
+        this.speed = 50
+
+        console.log("car speed", this.speed)
+
+    }
+}
+
