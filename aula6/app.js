bffb6e0cc367ac40436294df4fbabd9c00da8dfa
pc01\aluno <informatica@eticalgarve.com>
2024-01-22 19:35:16 +0000

    4to commit

--- aula6/app.js
+++ aula6/app.js
@@ -7,8 +7,25 @@
 //     hobbie: ("musica", "passear", "code")
 // }
 
-import Car from "../../Car.js"
-import Motorcycle from "../../Motorcycle.js"
+// import Car from "../../Car.js"
+// import Motorcycle from "../../Motorcycle.js"
 
-const car = new Car()
-const motorcycle = new Motorcycle()
+// const car = new Car()
+// const motorcycle = new Motorcycle()
+
+
+const loadData = async () => {
+ 
+
+    const request = await fetch("data.json")
+    const result = await request.json()
+
+    console.log(result)
+}
+
+window.onload = async () => {
+
+ const data = loadData();
+ console.log(data)
+
+}
