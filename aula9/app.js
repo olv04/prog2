ac0f7e33b5711265b824983e112f9e9b196b5d2e
Tomás Oliveira <146849187+olv04@users.noreply.github.com>
2024-01-25 21:54:26 +0000

    commit message

--- aula9/app.js
+++ aula9/app.js
@@ -1,20 +1,3 @@
-window.onload = async () => {
+console.log(1):
 
-    const test = new AppTest();
-    test.addEventListener('test_data_loaded', (event) =>{
-        console.log(event.detail);
-    })
-    test.loadData();
-}
-
-class AppTest extends HTMLElement {
-    constructor() {
-        super();
-    }
-    async loadData() {
-        const req = await fetch("data.json");
-        const res = await req.json();
-        this.dispatchEvent(new CustomEvent("test_data_loaded", {detail:res}));
-    }
-}
-customElements.define('app-test', AppTest);
+WindowLoad 
